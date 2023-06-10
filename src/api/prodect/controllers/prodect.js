4bbd287ef8dfd9ad5518da8d021150d76f578642
mohamed404eg <mohamed404eg@gmail.com>
2023-06-10 13:24:41 +0200

    frist

---
+++ src/api/prodect/controllers/prodect.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prodect controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::prodect.prodect');
